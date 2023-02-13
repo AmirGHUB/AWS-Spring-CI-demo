7f9f03165a3b589f677c1fc18d47e011d45a4b5d
Amir Abubeker <amirabubeker@Amirs-Mac-mini.attlocal.net>
2023-02-13 13:57:52 -0600

    added a new controller

--- src/main/java/com/aws/awsspringcidemo/controller/GreetController.java
+++ src/main/java/com/aws/awsspringcidemo/controller/GreetController.java
@@ -1,7 +1,10 @@
 package com.aws.awsspringcidemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 public class GreetController {
@@ -9,5 +12,14 @@ public class GreetController {
     @GetMapping("/greet")
     public String greetUser() {
         return "Hello user";
+    }
+
+    @GetMapping("/greet/{username}")
+    public String greetuserByName(@PathVariable("username") String username) {
+        if (!username.isBlank()) {
+            return "Hello " + username;
+        } else {
+            return "Invalid name entered.";
+        }
     }
 }
